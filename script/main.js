739d535fd19643dc239be46c06faa738b723cdb1
llwjdaks32 <112531911+llwjdaks32@users.noreply.github.com>
2022-10-06 16:11:45 +0900

    Add files via upload

--- script/main.js
+++ script/main.js
@@ -30,13 +30,28 @@ fadeEl.forEach(function(fadeEl,index){
 });
 });
 
-const promotionEl= document.querySelector("section.promotion");
-const promotionToggleBtn= document.querySelector(".n_inner__right>.material-icons");
-//토글 클릭시
-promotionToggleBtn.addEventListener('click',function(){
-    if(promotionEl.classList.contains('hide')) {
-        promotionEl.classList.remove('hide');
-    }else{
-        promotionEl.classList.add('hide');
+// const promotionEl= document.querySelector("section.promotion");
+// const promotionToggleBtn= document.querySelector(".n_inner__right>.material-icons");
+// //토글 클릭시
+// promotionToggleBtn.addEventListener('click',function(){
+//     if(promotionEl.classList.contains('hide')) {
+//         promotionEl.classList.remove('hide');
+//     }else{
+//         promotionEl.classList.add('hide');
+//     }
+// });
+
+//커피프로모션 토글아이콘 클릭시 박스 숨기고/ 나타내기
+
+const promotionEl = document.querySelector('section.promotion');
+const toggleBtnEl = document.querySelector('.toggle_btn');
+
+//토글버튼을 클릭하면 프로모션의 크기가 변한다.
+
+toggleBtnEl.addEventListener('click',function(){
+    if(promotionEl.classList.contains('hide')){//만약에 promotion클래스에 hide서식이 있다면 조건에 맞으면
+        promotionEl.classList.remove('hide')//높이 663이되어야
+    }else{//그렇지 않으면
+        promotionEl.classList.add('hide')//높이가 0원래대로
     }
 });
